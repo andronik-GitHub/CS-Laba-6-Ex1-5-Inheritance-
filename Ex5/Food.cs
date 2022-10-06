12ab00e9c3e9664e3a0746471d33adbc194fbc50
kazmi <costellationoblivion@gmail.com>
2022-10-06 22:20:52 +0300

    changeasdfa

--- Ex5/Food.cs
+++ Ex5/Food.cs
@@ -5,8 +5,9 @@ class Food
     public string? Name { get; set; }
     public string? Description { get; set; }
 
-    public Food (string? name)
+    public Food (string? name, string desctiption)
     {
         Name = name;
+        Description = desctiption;
     }
 }
