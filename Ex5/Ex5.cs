f161846f7a1c133b0dde4198450ef813d1020a66
kazmi <costellationoblivion@gmail.com>
2022-10-06 22:20:52 +0300

    changeasdfa

--- Ex5/Ex5.cs
+++ Ex5/Ex5.cs
@@ -4,6 +4,18 @@ class Ex5
 {
     static void Main()
     {
+        // Вводжу рядок з іжую через пробіли
+        string? temp = Console.ReadLine();
+
+
+        // Створюю об'єкт з їжею
+        FoodFactory foodFactory = new(temp);
+        foodFactory.Filling();
+
+        // Створюю об'єкт з настроєм
+        MoodFactory moodFactory = new();
+        moodFactory.Filling(foodFactory.Food);
+        moodFactory.OutputMood();
 
 
         Console.ReadKey();
--- Ex5/Person.cs
+++ Ex5/Person.cs
@@ -5,4 +5,5 @@ abstract class Person
     public abstract string? Importance { get; set; }
 
     public abstract void Filling();
+    public abstract void Filling(string? []obj);
 }
